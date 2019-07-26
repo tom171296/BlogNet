6d78ab5c02c4d19711f70678cdb6ac07074c7300
Tom van den Berg <t.vandenberg@infosupport.com>
2019-07-26 23:46:37 +0200

    initial commit frontend

---
+++ BlogNet/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OverviewComponent } from './overview/overview.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OverviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
